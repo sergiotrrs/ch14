317c133c0cc3577cec19a05f912986b5dfc08c23
Carla <soycarly2@gmail.com>
2022-06-17 15:17:24 -0500

    Puse un signo de admiración que me faltaba

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function cambiarSaludo() {
     //console.log("Me parece una falta de respecto");
     let frase = document.getElementsByTagName("h3")[0].innerHTML;
     if (document.getElementsByTagName("h3")[0].innerHTML == "Ganando como siempre") {
-        //frase = "Impongo moda para las niñas!";
+        //frase = "¡Impongo moda para las niñas!";
         document.getElementsByTagName("h3")[0].innerHTML = "Impongo moda para las niñas!";
     } else {
         //frase = "Ganando como siempre";
