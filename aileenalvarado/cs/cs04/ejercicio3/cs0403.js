d7a6f3c1ee656daf98299d3f99fc4c104302488d
Aileen Alvarado <monsealvarado775@gmail.com>
2022-06-17 18:17:07 -0500

    Cambios

---
+++ aileenalvarado/cs/cs04/ejercicio3/cs0403.js
@@ -0,0 +1,10 @@
+
+  
+let numb = [1, 2, 3, 4], suma = 0, multiplicacion = 1;
+for(let i=0; i<numb.length; i++){
+       suma += numb[i] ; 
+        multiplicacion *= numb[i] ;
+  }   
+console.log("suma " + suma);
+console.log("multiplicacion " + multiplicacion);
+  
