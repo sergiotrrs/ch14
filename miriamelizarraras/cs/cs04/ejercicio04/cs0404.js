d87eb39548174c40d80d8503a993ea30ca6cea84
Miriam Elizarrarás <hello.tarantino@gmail.com>
2022-06-18 18:27:09 -0500

    se agregaron trabajos completos del 1 a 3

---
+++ miriamelizarraras/cs/cs04/ejercicio04/cs0404.js
@@ -0,0 +1,3 @@
+/* let student1Courses = ['Math', 'English', 'Programming'];
+let student2Courses = ['Geography', 'Spanish', 'Programming'];
+Create a program that loops over the 2 arrays; if there are any common courses print them out to the console. */
