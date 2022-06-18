a4cd8df887e370fcef14ae070bb9793073e30a74
Jose Estrella <jcruz_98@outlook.com>
2022-06-18 16:49:34 -0500

    Fin mine

---
+++ joseestrella/cs/cs03/ejercicio01/cs0301.js
@@ -0,0 +1,20 @@
+console.log("Acá pongo mi código");
+/*
+Aquí merito, por favor.
+*/
+//VARIABLES
+let num1 = 10;
+let num2 = 5;
+let num3 = 15;
+
+//1.Solicitar 3 números (entre el 1 y el 100)  y definir cuál es el mayor
+if (num1 > num2 && num1 > num3) {
+    console.log("El numero mayor es el num1");
+} else if (num2 > num1 && num2 > num3) {
+    console.log("El numero mayor es el num2");
+} else if (num3 > num1 && num3 > num2) {
+    console.log("El numero mayor es el num3");
+} else {
+    console.log("Error");
+}
+//--------------------------------------------------------------------------------------------------------------------
---
+++ joseestrella/cs/cs03/ejercicio04/cs0304.js
@@ -0,0 +1,14 @@
+console.log("Acá pongo mi código");
+/*
+Aquí merito, por favor.
+*/
+//VARIABLES
+let num4 = 150;
+
+//4.Solicitar un entero (entre el 100 y el 200) y determinar si es múltiplo de 3
+if (num4 % 3 == 0) {
+    console.log("Este numero si es multiplo de 3");
+} else {
+    console.log("Este numero no es multiplo de 3");
+}
+//--------------------------------------------------------------------------------------
