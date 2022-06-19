b2dce95d34dd2c876e12e3183f98449cfe962f7b
Karla Cristina Flores <karlacristinafloresh@gmail.com>
2022-06-19 17:18:52 -0500

    Se agrega ejercicio 01 de cs03

---
+++ karlaflores/cs/cs03/ejercicio01/ejerciciocs01.js
@@ -0,0 +1,36 @@
+console.log("Código de CS 01")
+
+/*ejercicio 1
+Solicitar 3 números entre el 1 y 100, definir el mayo.
+como puedo usar una funcion para elegir números aleatorios*/
+
+let A= 1;
+let B= 2;
+let C= 3;
+
+if (A>B && A>C) {
+    console.log("El número mayor es " + A);
+}
+
+else if (B>A && B>C) {
+    console.log("El número mayor es " + B);
+}
+
+else if (C>A && C>B); {
+    console.log("El número mayor es " + C);
+}
+
+//ejercicio 2 Solicitar 3 números entre el 1 y 100, definir el menor.
+
+if (A<B && A<C) {
+    console.log("El número menor es " + A);
+}
+
+else if (B<A && B<C) {
+    console.log("El número menor es " + B);
+}
+
+else if (C<A && C<B) {
+    console.log("El número menor es " + C);
+}
+
