375180a6275e22eff3a69c4bd27f00b1154da5c2
Lucia Cortes <luciacortesmolina@gmail.com>
2022-06-20 14:15:00 -0500

    upload: ejercicios

---
+++ luciacortes/cs/cs03/ejercicio06/Ej.6_numParoImpar.js
@@ -0,0 +1,12 @@
+/*Ejercicio 6. Elabora un algoritmo para leer un número 
+ y determinar si es par o impar
+*/
+
+let n = prompt("Escribe un número de 1 a 100");
+
+if ((n % 2) == 0) {
+ console.log ("El número " +n+ " es par");
+}
+else {
+ console.log ("El número " +n+ " es impar")
+}
