b51c9129d77432a87f87c70bc340fc3dc2f6053e
Karla Cristina Flores <karlacristinafloresh@gmail.com>
2022-06-19 22:58:49 -0500

    Se sube ejercicio 04 de cs03

---
+++ karlaflores/cs/cs03/ejercicio04/ejerciciocs0304.js
@@ -0,0 +1,12 @@
+console.log("Codigo 04 de CS03")
+
+//ejercicio 4 Solicitar entero entre 100 y 200, determinar si es múltiplo de 3
+
+let numberA= 157;
+
+if (numberA % 3 == 0) {
+  console.log("Es múltiplo de 3");
+}
+else {
+  console.log("No es múltiplo de 3")
+}
