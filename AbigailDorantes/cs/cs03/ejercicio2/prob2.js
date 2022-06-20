ae66dc9cead4e110dd6d5a8866ad3784b62b8936
Abigail Dorantes <abydorantes@hotmail.com>
2022-06-20 00:34:17 -0500

    Se agregan ejercicios correspondientes a cs03

---
+++ AbigailDorantes/cs/cs03/ejercicio2/prob2.js
@@ -0,0 +1,15 @@
+/*2.	Solicitar 3 números (entre el 1 y el 100)  y definir el menor de tres números*/
+
+let num1 = 15;
+let num2 = 88;
+let num3 = 23;
+let numMenor;
+
+if (num1 < num2) {
+    numMenor = num1;
+} else { num = num2 }
+if (numMenor < num3) {
+    numMenor = numMenor;
+} else { numMenor = num3; }
+
+console.log("El numero menor es: " + numMenor);
