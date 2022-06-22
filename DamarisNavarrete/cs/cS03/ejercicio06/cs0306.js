2301706953df918d5d22eadd95edc5a96c8208ad
Damaris Navarrete <navarretedamaris445@gmail.com>
2022-06-21 20:20:36 -0500

    se siguen acomodando carpetas

--- DamarisNavarrete/cs/cS03/ejercicio06/cs0306.js
+++
@@ -1,9 +0,0 @@
-let numero = 0;
-
-numero = prompt('Indique un número: ', '');
-
-if((numero % 2) == 0){
-  console.log(numero + ' es par');
-}else{
-  console.log(numero + ' es impar');
-}
--- DamarisNavarrete/cs/cS04/ejercicio02/cs0402.js
+++
@@ -1,9 +0,0 @@
-let arr=[1,2,4,5];
-let array=[];
-for (let i=0; i < array.length; i++){
-    array[1] = arr[1]*2
-}
-console.log (array);
-let array2=[]
-arr.forEach(dato => array2.push(dato*2));
-console.log(array2);
