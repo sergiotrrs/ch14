37ad37cd4c64a7d157b34296ad297d64ae714ee3
MaxEMorin <107515659+MaxEMorin@users.noreply.github.com>
2022-06-26 18:41:47 -0500

    Delete MaxMorin directory

--- MaxMorin/cs/cs04/ejercicio1/ejercicio1.js
+++
@@ -1,6 +0,0 @@
-let arr = ["This", "is", "a", "sentence."];
-function printOutString(a) {
-  console.log("" + a);
-}
-
-arr.forEach(printOutString);
--- MaxMorin/cs/cs04/ejercicio2/ejercicio2.js
+++
@@ -1,8 +0,0 @@
-let num = [1, 2, 4, 5];
-function printOutString(a){
-  for (let i = 0; i < a.length; i++){
-    let b=a[i]*2;
-    console.log("pos"+i, " = "+b);
-  }
-}
-printOutString(num);
