5f266152d570fc1366c523ac62ecb31dc82222c9
Montse Morales <jessica.morales5556@alumnos.udg.mx>
2022-06-27 22:20:54 -0500

    Subir: Practica Bootstrap

--- jessicamorales/web/web07/Ejercicio/scripts/cambiar.js
+++ jessicamorales/web/web07/Ejercicio/scripts/cambiar.js
@@ -1,5 +1,5 @@
 
 function cambiarNombre(element, color) {
     let name = prompt("¿Cuál es tu nombre?")
-    document.getElementById("name").innerHTML = "Tu nombre es " + name;
-  }
+    document.getElementById("name").innerHTML = "Hola " + name;
+}
