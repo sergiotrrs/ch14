a0e4ffb1081f0f7d51c88b4d703c943e53a8bbc0
JA Gonzalez <jatgonzalez33@gmail.com>
2022-06-29 16:58:01 -0500

    Upload: Practicas del 29/06/22 @GenMX

--- jandresgonzalez/JS/js03/teoria/script.js
+++ jandresgonzalez/JS/js03/teoria/script.js
@@ -13,9 +13,9 @@ let nombre = "Santis";
 {
     // Código en un bloque
     //Este flujo de trabajo tiene su propio variable scope
-    let nombre="ValeriaMojica"
+    let nombre = "ValeriaMojica"
     //variable scope de var es más grande, sin importan declaración de bloque
-    var apellido="Gayosso";
+    var apellido = "Gayosso";
 
     console.log(nombre);
 }
@@ -58,4 +58,111 @@ else if (edad < 15 && edad > 6) {
 }
 else {
     console.log("Es un_ baby");
+}
+
+/* Se vería así, en realidad
+if (edad >= 18) {
+    console.log("Mayor de edad");
+}
+else
+    if (edad >= 15 && edad < 18) {
+        console.log("Adolescente");
+    } else
+        if (edad < 15 && edad > 6) {
+            console.log("Es un niñ__");
+        } else {
+    console.log("Es un_ baby");
+            }
+*/
+
+/**
+ * SWITCH
+ * Para hacer SWITCH
+ * Se realiza un menú de operadora de call center
+ * como ejemplo
+ */
+
+let opcionElegida = 2;
+/*Para switch, la opción se evalúa como === estrictamente igual,
+en valor y tipo */
+switch (opcionElegida) {
+    case 1: console.log("Se eligió uno, para Ventas");
+        break;
+    //se estructura case 2 por bloques
+    case 2: {
+        console.log("Se eligió dos, para Asistencia");
+        let submenu = 2
+        switch (submenu) {
+            case 1: console.log("Seleccionó uno en submenu");
+                break;
+            case 2: console.log("Selección dos en submenu");
+                break;
+            default: console.log("Inválido en submenú");
+                break;
+        }
+        break;
+    }
+
+    case 0: console.log("Se eligió cero, para Operador_");
+        break;
+    default: console.log("Opción inválida. Repetir menú.");
+        break;
+}
+
+/*Use switch instead of if when:
+
+You are comparing multiple possible conditions of an expression
+and the expression itself is non-trivial.
+You have multiple values that may require the same code.
+You have some values that will require essentially
+all of another value's execution, plus only a few statements.
+Use if instead of switch when:
+
+You want to test for the truthiness of an expression.
+You only have a single affirmative test.
+You need to evaluate different expressions for each branch */
+
+let Animal = 'Jirafa';
+switch (Animal) {
+    case 'Vaca':
+    case 'Jirafa':
+    case 'Perro':
+    case 'Cerdo':
+        console.log('Este animal subirá al Arca de Noé.');
+        break;
+    //Todo esto se ejecuta si hay estos animales arriba
+    //En caso de que sea dinosaurio o cualquier otro, no suben al arca
+    case 'Dinosaurio':
+    default:
+        console.log('Este animal no lo hará.');
+}
+
+
+/**
+ *-------- Conditional Ternary Operator -------------
+ */
+
+ edad = 5;
+ let esMayor = (edad >= 18) ? "Puede votar" : "No puede votar";
+ console.log("¿Puede votar? " + esMayor);
+
+ /*let stop = false, age = 23;
+
+age > 18 ? (
+    alert("OK, puedes continuar."),
+    location.assign("continue.html")
+) : (
+    stop = true,
+    alert("Disculpa, eres menor de edad!")
+); */
+
+//Otro if
+
+let age = "Montse";
+let voteable;
+age = Number(age);
+if (isNaN(age)) {
+  voteable = console.log("Input is not a number");
+} else {
+  voteable = (age < 18) ? console.log("Too young") : console.log("Old enough");
 }
