a3f2c5f5b2e59f68aee2b67af69340b02065afb4
Julio Hernández <julioergocesar@outlook.com>
2022-06-30 15:21:49 -0500

    mod:Eliminar la cola

--- juliohernandez/js/js04/Teoria/script.js
+++ juliohernandez/js/js04/Teoria/script.js
@@ -4,3 +4,9 @@ console.log("Sesión JS04");
 const miArreglo=["dato String", false, 23, null, true];
 console.log(miArreglo.toString()); // 'dato String,false,23,,true
 console.log(miArreglo.join("-")); // 'dato String-falsee-23--true
+
+//Eliminar último dato del arreglo y pasarlo a una variable**
+const frutas=["Mango","Plátano","Melón", "Guanábana", "Aguacate","Kiwi"];
+let fruta = frutas.pop(); // .pop() es un método
+console.log(frutas.join(" * "));
+console.log(`Fruta eliminada: ${fruta}`)  //¡¡NO regresa un string!!
