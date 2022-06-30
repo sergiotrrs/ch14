e62ad4a28714dcae0428cbbfe0ade7f4631e8e12
Miriam Elizarrarás <hello.tarantino@gmail.com>
2022-06-30 16:08:29 -0500

    add: Carpeta js02 con archivos de bootstrap, calculadora index.html y calculadora.js

---
+++ miriamelizarraras/js/js04/script.js
@@ -0,0 +1,17 @@
+//Como definir un arreglo ****
+
+const miArreglo = ["dato String", false, 23, null, {a:12, b:34}];
+console.log(miArreglo);
+console.log(miArreglo.toString());
+console.log(miArreglo.join("-"));
+
+//Eliminar el último dato del arreglo y pasarlo a una variable**
+const frutas=["mango", "plátano", "melón", "Guanábana", "aguacate", "kiwi"];
+let fruta = frutas.pop(); // Se elimina el último elemento del arreglo.//Devuelve el elemento eliminado.
+console.log(frutas.join("*"));
+console.log("Fruta eliminada " + fruta );
+
+//Agregar un elemento al final del arreglo ****
+
+console.log(frutas.push("uvas")); //Agregamos el elemento al final del arreglo // Devuelve la longitud del arreglo.
+console.log(frutas.join(" - "));
