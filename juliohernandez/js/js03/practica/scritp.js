21ff2be1e4639c7a2fccb9fc7fab5a74e0f887b8
Julio Hernández <julioergocesar@outlook.com>
2022-06-30 12:53:57 -0500

    release: diaLaboraloNo v.1

---
+++ juliohernandez/js/js03/practica/scritp.js
@@ -0,0 +1,26 @@
+let a = 1900;
+let m = 1;
+let d = 1;
+let respuesta;
+
+let weekday = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+
+document.getElementById("calcula").onclick = function () {
+    a = Number(document.getElementById("año").value);
+    m = Number(document.getElementById("mes").value);
+    d = Number(document.getElementById("dia").value);
+
+
+    let fecha = new Date(a, m - 1, d);
+    let dia = weekday[fecha.getDay()];
+
+    switch (fecha.getDay()) {
+        case 0: case 6:
+            respuesta = `${dia}, día no laboral`;
+            break;
+        default:
+            respuesta = `${dia}, día laboral. Sorry.`;
+    }
+
+    document.getElementById("resultado").value = respuesta;
+}
