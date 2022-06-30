ecc57014347cefec77eb72d9e4371af858d2294f
Julio Hernández <julioergocesar@outlook.com>
2022-06-30 15:07:09 -0500

    mod:Definir un arreglo

--- juliohernandez/js/js04/Teoria/script.js
+++ juliohernandez/js/js04/Teoria/script.js
@@ -1,1 +1,6 @@
-console.log("Sesión JS04")
+console.log("Sesión JS04");
+
+//Cómo definir un arreglo
+const miArreglo=["dato String", false, 23, null, true];
+console.log(miArreglo.toString()); // 'dato String,false,23,,true
+console.log(miArreglo.join("-")); // 'dato String-falsee-23--true
