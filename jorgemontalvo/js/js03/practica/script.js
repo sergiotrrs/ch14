1b84344a5c62b975ea124e6c3d3d362a94a8b162
Jorge Montalvo <jorgemontalvo96@gmail.com>
2022-07-02 17:17:19 -0500

    mod: se hace responsiva el calendario

--- jorgemontalvo/js/js03/practica/script.js
+++ jorgemontalvo/js/js03/practica/script.js
@@ -1,29 +1,34 @@
 console.log("Aquí inicia el programa");
+let year;
+let month;
+let day;
 
+function calcular() {  
 // Entradas del Usuario año, mes, día. El mes se le agrega "-1" porque el index es Enero
-    let year = document.getElementById("anio").value;
-    let month = document.getElementById("mes").value - 1;
-    let date = document.getElementById("fecha").value;
+    year = Number(document.getElementById("anio").value);
+    month = Number(document.getElementById("mes").value)-1;
+    date = Number(document.getElementById("fecha").value);
 
     // Evaluamos el día .getDay() devuelve un numero entre 0-6
-    const dia = new Date(year, month, date).getDay();
+    let dia = new Date(year, month, date,) ;
+    let fecha = dia.getDay();
     console.log("Número de día de la semana: ", dia);
-    
+    console.log(Date(year, month, date))
+    console.log(fecha)
     
 //Funcion que contiene el switch y los distintos casos
-    function calcular() {  
-    switch (dia) {
+    switch (fecha) {
         case 0:
             document.getElementById("resul").innerHTML = "El día es Domingo y no es laborable ";
             break;
         case 1:
-            document.getElementById("resul").innerHTML = "El día es Domingo y es laborable ";
+            document.getElementById("resul").innerHTML = "El día es Lunes y es laborable ";
             break;
         case 2:
-            document.getElementById("resul").innerHTML = "El día es Domingo y es laborable ";
+            document.getElementById("resul").innerHTML = "El día es Martes y es laborable ";
             break;
         case 3:
-            document.getElementById("resul").innerHTML = "El día es Domingo y es laborable ";
+            document.getElementById("resul").innerHTML = "El día es Miércoles y es laborable ";
             break;
         case 4:
             document.getElementById("resul").innerHTML = "El día es Jueves y es laborable ";
