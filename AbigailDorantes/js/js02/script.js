7046af275c927c01017ab22d222f0161af2f793c
Abigail Dorantes <abydorantes@hotmail.com>
2022-07-04 03:07:05 -0500

    Se agrega modificacion al archivo index.html

--- AbigailDorantes/js/js02/script.js
+++ AbigailDorantes/js/js02/script.js
@@ -3,7 +3,7 @@ console.log("la suma es: " + suma());*/
 
 //Se agregan funciones para realizar operaciones suma, resta,  mutiplicacion y division 
 
-function sumar(){
+const sumar=()=>{
     let a= parseFloat(document.getElementById('num1').value);
     let b= parseFloat(document.getElementById('num2').value);
     document.getElementById("result").innerHTML = a+b;
