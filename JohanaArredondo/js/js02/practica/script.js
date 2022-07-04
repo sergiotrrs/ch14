ef070dd1a1f61e50eeb14c131a58b2c55c21616f
Johana Arredondo <johana.arredondo3402@alumnos.udg.mx>
2022-07-04 03:45:16 -0500

    prueba

--- JohanaArredondo/js/js02/practica/script.js
+++ JohanaArredondo/js/js02/practica/script.js
@@ -13,6 +13,7 @@ function multiplica(n1 = parseFloat(document.getElementById("n1").value), n2 = p
     document.getElementById("res").value = n1 * n2;
 };
 
+//nuevo
 const div = (n1 = parseFloat(document.getElementById("n1").value), n2 = parseFloat(document.getElementById("n2").value)) => document.getElementById("res").
 value = n1/n2;
 
