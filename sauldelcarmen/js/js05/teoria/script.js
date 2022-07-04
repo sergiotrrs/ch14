31101a82ab33f8703916d6c065280815505f89cc
root <root@Sharvide.localdomain>
2022-07-04 17:25:14 -0500

    Add: JS05,JS06

--- sauldelcarmen/js/js05/teoria/script.js
+++ sauldelcarmen/js/js05/teoria/script.js
@@ -122,3 +122,88 @@ x *=2 ;//x=x*2 -> x=14
 x /=2 ;//x=x/2 -> x=7
 x %=2 ;//x=x%2 -> x=1
 x **=2 ;//x=x**2 -> x=4
+
+/**
+ *Operadores de igualdad (comparacion)
+ */
+//Operador igual que
+x=1;
+y=2;
+x == y; //false
+y=1;
+x == y; //true
+x=1;
+y="1";
+//true, compara el valor, el tipo string se convierte en numerico
+console.log(" x == 'y'+"+ (x == y));
+//Operador diferente que !=
+x = 1;
+y = '2';
+console.log(" x != 'y' "+ (x != y));//true 1 != '2'
+console.log(" x++ != 'y' "+ (x++ != y));//true 1 != '2' -> x=x+1 -> x=2
+console.log(" x != 'y' "+ (x != y));//false 2 != '2'
+console.log(" ++x != 'y' "+ (++x != y));//true 2 != '2' -> x=x+1 -> x=3
+//Operador extrictamente igual que === (compara el valor y el tipo)
+x = 1;
+y = '1';
+console.log(" x === 'y' "+ (x === y));//false
+//Operador extrictamente diferente que !== (compara el valor y el tipo)
+x = 1;
+y = '1';
+console.log(" x !== 'y' "+ (x !== y));//true
+//Operadores logicos &&, ||, !
+true && true;//true
+true && false;//false 
+//False, null, NaN, 0,empty string, undefined son considerados con valor false:
+//Si el primer operando es false, el segundo operando no se evalua y se devuelve el primer operando
+//Si el primer operador es true, el resultado es el valor del segundo operador
+console.log("true && NaN = "+ (true && NaN));//NaN
+console.log("true && NaN = "+ (false && NaN));//false
+console.log("true && 'Hola estrella' = "+ (true && 'Hola estrella'));//Hola estrella
+console.log("null && 'Hola Moi' = "+ (null && 'Hola Moi'));//null
+console.log("'a' && 'Hola Moi' = "+ ('a' && 'Hola Moi'));//Hola Moi
+console.log("' ' && 'Hola Maga' = "+ ('' && 'Hola Maga'));//'' (empty string)
+console.log("' ' && 'Hola Maga' = "+ (' ' && 'Hola Maga'));//Hola Maga
+
+//Operador OR ||
+true || false;//true
+//False, null, NaN, 0,empty string, undefined son considerados con valor false.
+//Si el primer operando es true, el segundo operando no se evalua y se devuelve el primer operando
+//Si el primer operador es false, el resultado es el valor del segundo operador
+console.log("\n Operador or ||");
+console.log("true || NaN = "+ (true || NaN));//true
+console.log("true || NaN = "+ (false || NaN));//NaN
+console.log("true || 'Hola estrella' = "+ (true || 'Hola estrella'));//true
+console.log("null || 'Hola Moi' = "+ (null || 'Hola Moi'));//Hola Moi
+console.log("'a' || 'Hola Moi' = "+ ('a' || 'Hola Moi'));//a
+console.log("' ' || 'Hola Maga' = "+ ('' || 'Hola Maga'));//Hola Maga
+console.log("' ' || 'Hola Maga' = "+ (' ' || 'Hola Maga'));//Hola Maga
+
+//operador NOT !
+let nuevoValor = !true;//false
+nuevoValor = !false;//true
+//False, null, NaN, 0,empty string, undefined son considerados con valor false.
+//Si el operando es true, se devuelve false
+//Si el operador es false, se devuelve true
+console.log("\n Operador or !");
+console.log(" !NaN = "+ (!NaN));//true
+console.log("!true = "+ (!true));//false
+console.log(" !'Hola estrella' = "+ (!'Hola estrella'));//false
+console.log(" !false = "+ (!false));//true
+console.log(" !'a' = "+ (!'a'));//false
+console.log("!'' = "+ (!''));//true
+console.log(" !' ' = "+ (!' '));//false
+
+//Operador logico binario ?? nullish coalescing
+//Operador logico que retorna el operador de la derecha, si el operador
+//de la izquierda es null o undefined. De lo contrario, se devuelve el operador de la izquierda
+let asistencia;
+console.log("\n Operador ?? nullish coalescing");
+console.log("asistencia = null ?? true :" +(asistencia = null ?? true));//true
+console.log("asistencia = 1 ?? true :" +(asistencia = 1 ?? true));//1
+x = undefined;
+console.log("Asistencia de asistencia =x :" +(asistencia = x ?? "Asiste"));//Asiste
+console.log("Asistencia de asistencia =x :" +(asistencia = '' ?? "Asiste"));
+//Si la izquierda es null o undefined, se retorna la derecha
+//Si la izquierda NO ES null o undefined, se retorna la izquierda
+
