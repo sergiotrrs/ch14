2d7566d848c37ed31ed2d5a05b43b61dfd4fa7ec
Johana Arredondo <johana.arredondo3402@alumnos.udg.mx>
2022-07-04 04:59:18 -0500

    final

--- JohanaArredondo/js/js02/practica/script.js
+++ JohanaArredondo/js/js02/practica/script.js
@@ -1,19 +1,23 @@
+//Funcion suma
 function suma(n1 = parseFloat(document.getElementById("n1").value), n2 = parseFloat(document.getElementById("n2").value)){
 
     document.getElementById("res").value = n1 + n2;
 };
 
+//Funcion resta
 function resta(n1 = parseFloat(document.getElementById("n1").value), n2 = parseFloat(document.getElementById("n2").value)){
 
     document.getElementById("res").value = n1 - n2;
 };
 
+//Funcion multiplicacion
 function multiplica(n1 = parseFloat(document.getElementById("n1").value), n2 = parseFloat(document.getElementById("n2").value)){
 
     document.getElementById("res").value = n1 * n2;
 };
 
-//nuevo
+
+//Funcion division con arrow function
 const div = (n1 = parseFloat(document.getElementById("n1").value), n2 = parseFloat(document.getElementById("n2").value)) => document.getElementById("res").
 value = n1/n2;
 
