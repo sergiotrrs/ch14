0ae7ae7110cca0149d035bee30149c029e4b3387
Azucena Ceniceros <azucg99@gmail.com>
2022-07-04 01:10:50 -0500

    Añadí estilos bootstrap al archivo de calculadora

--- azucenacg/js/js02/practica/script.js
+++ azucenacg/js/js02/practica/script.js
@@ -1,9 +1,9 @@
 // Esta función nos permite leer los datos de las 2 entradas
 // Se manda a llamar para las funciones de cada botón
 function leerDatos() {
-    const v1= parseInt(document.getElementById("valor1").value);
+    const v1= Number(document.getElementById("valor1").value);
     console.log(v1);
-    const v2= parseInt(document.getElementById("valor2").value);
+    const v2= Number(document.getElementById("valor2").value);
     console.log(v2);
     return {v1:v1, v2:v2}
 }
@@ -16,34 +16,36 @@ let suma = () =>{
     valor=leerDatos();
     let sum=valor.v1+valor.v2;
     console.log("El valor de la suma es: "+ sum);
-    document.getElementById("resul").innerHTML=sum;
+    document.getElementById("resul").value=sum;
 }
 
 let resta = () =>{
     valor=leerDatos();
     let res=valor.v1-valor.v2;
     console.log("El valor de la resta es: "+ res);
-    document.getElementById("resul").innerHTML=res;
+    document.getElementById("resul").value=res;
 }
 
 let division = () =>{
     valor=leerDatos();
     let div=valor.v1/valor.v2;
     console.log("El valor de la división es: "+ div);
-    document.getElementById("resul").innerHTML=div;
+    document.getElementById("resul").value=div;
 }
 
 let multiplica = () =>{
     valor=leerDatos();
     let mul=valor.v1*valor.v2;
     console.log("El valor de la multiplicación es: "+ mul);
-    document.getElementById("resul").innerHTML=mul;
+    document.getElementById("resul").value=mul;
 }
 
 
 // Función que borra los datos en pantalla
 let limpia = () =>{
-    document.getElementById("resul").innerHTML= 0 ;
+    document.getElementById("valor1").value= " ";
+    document.getElementById("valor2").value= " ";
+    document.getElementById("resul").value= " ";
 }
 
 
