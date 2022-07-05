0497d8591f1d0d0799ed1871329e8b32d4076176
JA Gonzalez <jatgonzalez33@gmail.com>
2022-07-05 13:23:20 -0500

    Modificado: Inputs con filtros

--- jandresgonzalez/JS/js06/practica/practica/script/script.js
+++ jandresgonzalez/JS/js06/practica/practica/script/script.js
@@ -6,13 +6,62 @@
  * @return fecha dd/mm/aaaa (fecha solicitada)
  */
 const calcularFecha = () =>{ 
-    //Se manda llamar a la función Date() que se almacena en la constante día
+/*     //Se manda llamar a la función Date() que se almacena en la constante día
     const dia = new Date(Number(document.getElementById("anio").value),String(document.getElementById("mes").value)-1, Number(document.getElementById("dia").value));
     console.log(dia);
     //Se coloca -1 en el mes por el año bisiesto 
     document.getElementById("fecha").value = definirFecha(dia.getDay()); //Se manda llamar a la función definirFecha 
+ SHIFT ALT A pa comentar*/
 
+const formulario = document.getElementById("form");
+let day = formulario.elements['dia'].value;
+let month = formulario.elements['mes'].value - 1;
+let year = formulario.elements['anio'].value;
+const dia = new Date(
+    year,
+    month,
+    day
+);
+
+if (formulario.elements['anio'].value == 0 || formulario.elements['dia'].value == 0) {
+    window.alert("Por favor, ponga fecha en los campos indicados.");
 }
+else {
+/*
+else {
+    if (month == 1 || 
+        month == 3 ||
+        month == 5 ||
+        month == 7 || 
+        month == 8 ||
+        month == 10 ||
+        month == 12 &&
+        day >=32) {
+        window.alert(`El mes ingresado tiene 31 días. Por favor, ingrese otro día`);
+        day = 31;
+    }
+    else if (month == 4 ||
+            month==6 ||
+            month==9 ||
+            month==11 &&
+            day >=31) {
+        window.alert(`El mes ingresado tiene 30 días. Por favor, ingrese otro día`);
+        day = 30;
+    }
+    else if (month == 2) {
+        if (year%4 == 0) {
+            window.alert(`El mes ingresado tiene 29 días en bisiesto. Por favor, ingrese otro día`);
+            day = 29;
+        }
+        else {
+            window.alert(`El mes ingresado tiene 28 días, si el año no es bisiesto. Por favor, ingrese otro día`);
+            day=28;
+        }
+    } */
+    formulario.elements['fecha'].value = definirFecha(dia.getDay());
+}
+}
+
 
 //Función para indicar el día y si es laborable
 /**
