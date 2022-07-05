683d0bc7035250d1cc09c155e2cfa1d1a87c5f82
Julio Hernández <julioergocesar@outlook.com>
2022-07-05 15:47:09 -0500

    Reestructuración

--- juliohernandez/js/js06/practica/script/script.js
+++ juliohernandez/js/js06/practica/script/script.js
@@ -5,15 +5,16 @@
  * @param {int} dia
  * @return fecha dd/mm/aaaa (fecha solicitada)
  */
-const calcularFecha = () =>{ 
-    //Se manda llamar a la función Date() que se almacena en la constante día
-    /* const dia = new Date(
-        Number(document.getElementById("anio").value),
-        String(document.getElementById("mes").value)-1, 
-        Number(document.getElementById("dia").value)); 
-     
-   document.getElementById("fecha").value = definirFecha(dia.getDay()); //Se manda llamar a la función definirFecha 
-   */
+const calcularFecha = () => {
+    const formulario = document.getElementById("formulario");
+    console.log(formulario.elements['mes'].value);  //con ID ó name.
+    const dia = new Date(
+        formulario.elements['anio'].value,
+        formulario.elements['mes'].value - 1,
+        formulario.elements['dia'].value
+    );
+
+    formulario.elements['fecha'].value = definirFecha(dia.getDay());
 }
 
 //Función para indicar el día y si es laborable
@@ -22,29 +23,29 @@ const calcularFecha = () =>{
  * @param {int}  dia 
  * @return "'Día', fin de semana o 'Día', día lavorable"
  */
-function definirFecha(dia){
-    switch(dia){
+function definirFecha(dia) {
+    switch (dia) {
         //Se inicializa 0 porque Date() comprende los día del 0 al 6
-        case 0: 
+        case 0:
             return "Domingo, fin de semana";
-               
+
         case 1:
             return "Lunes,  día laborable";
-            
+
         case 2:
             return "Martes,  día laborable";
-            
+
         case 3:
             return "Miércoles,  día laborable";
-            
+
         case 4:
             return "Jueves,  día laborable";
-            
+
         case 5:
             return "Viernes, día laborable";
-            
+
         case 6:
             return "Sábado, fin de semana";
-            
+
     }
 }
