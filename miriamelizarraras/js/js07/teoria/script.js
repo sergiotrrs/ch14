f295ac6baf6823276ec5a8e0519cd23db1db9ab4
Miriam Elizarrarás <hello.tarantino@gmail.com>
2022-07-05 23:20:40 -0500

    add: js07/

---
+++ miriamelizarraras/js/js07/teoria/script.js
@@ -0,0 +1,40 @@
+let x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML =
+    "Latitude: " +
+    position.coords.latitude +
+    "<br>Longitude: " +
+    position.coords.longitude;
+}
+
+function guardarDatos() {
+  const formulario = document.getElementById("formulario");
+  console.log("nombre: " + formulario.elements["nombre"].value);
+  console.log("apellido: " + formulario.elements["apellido"].value);
+  //Se le indica una clave y un valor
+  localStorage.setItem("Cohorte", "Ch14");
+  localStorage.setItem("nombre", formulario.elements["nombre"].value);
+  localStorage.setItem("apellido", formulario.elements["apellido"].value);
+  localStorage.setItem("datosUsuario", {
+    "nombre": formulario.elements["nombre"].value,
+    "apellido": formulario.elements["apellido"].value,
+  }
+  )
+}
+
+function recuperarDatos() {
+  const formulario = document.getElementById("formulario");
+  formulario.elements["nombre"].value = localStorage.getItem("nombre");
+  formulario.elements["apellido"].value = localStorage.getItem("apellido");
+  let datos = localStorage.getItem("datosUsuario");
+  console.log("Datos usuario: " + ${datos.nombre} + ${datos.apellido})
+}
