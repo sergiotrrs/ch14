e58ba520b1eae444de90f9aa99cc6d2db541b6a6
Luis Medina <luisdavid220800@gmail.com>
2022-07-06 01:45:30 -0500

    agregar: archivos de practica

--- luismedina/js/js05/script.js
+++ luismedina/js/js05/script.js
@@ -84,3 +84,61 @@ x *= 3; // x = x * 3
 x /= 2; // x = x/2
 x %= 4; // x = x % 4
 x **= 3; // x = x**3
+
+//true, compara el valor, el tipo string se convierte en numerico
+console.log("x ==`y`" + (x == y));
+//operador difrente que !=
+
+x = 1;
+y = `2`;
+console.log("x != `y`" + (x != y)); // true 1 != `2`
+console.log("x++ != `y`" + (x++ != y)); // true 1 != `2` => x=2
+console.log("x != `y`" + (x != y)); // false 2 != `2`
+console.log("++x != `y`" + (++x != y)); // 3 != `2` => true
+
+//operador estrictamente igual
+x = 1;
+y = `1`;
+console.log(" x === `y` " + (x === y));//false
+//operador estrictamente diferente que !== compara el valor y tipo de dato
+console.log("x !== `y`" + (x !== y));//true
+
+//operadores logicos &&, ||, !
+true && true // true
+true && flase // si uno de los valores es false el resultado sera falso
+//Si el primer operador es true, el resultado es el valor del segundo operador.
+//operadores de corto circuito
+console.log("true && NaN" + (true && NaN));//NaN
+console.log("true && NaN" + (false && NaN));//false
+console.log("true && `Hola`" + (true && NaN));//Hola
+console.log("null && `Hola`" + (true && NaN));//Null
+console.log("`a` && `Hola`" + (false && NaN));//Hola
+console.log("`` && `Hola`" + (true && NaN));//`` String vacio
+
+//operador logico or ||
+true || false // true
+//false, null, NaN, 0, empty string o undefined son considerados von valor false.
+//Si el primer operador es true ek resultado es el primer operador
+//Si el primer operador es false, el resultado es el segundo operador.
+console.log("\n Operador ||");
+console.log("true || NaN" + (true || NaN));//true
+console.log("false || NaN" + (false || NaN));//Nan
+console.log("true || `Hola`" + (true || "hola"));//true
+console.log("null || `Hola`" + (null || "Hola"));//Null
+console.log("`a` || `Hola`" + (`a` || `Hola`));//a
+console.log("`` || `Hola`" + (`` || `Hola`));//`` hola
+
+//Operador logico not !
+let nuevoValor = !true //false
+nuevoValor = !false // true
+//false, null, NaN, 0, empty string o undefined son considerados von valor false.
+
+
+
+//Operdaor logico binario ?? nullish Coalescing
+//operdaor logido que retorna el valor de la derecha si el operador de la izquierda en null o undefined de lo contrario restorna el operador de la izquierda
+
+
+
+
+// Los datos se guardan en memoria como 1 y 0
