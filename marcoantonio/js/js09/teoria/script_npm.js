0eca884f9ecfd4a9410b2eb34ce67e6461cedd0b
marcantonio13 <antonio.avellaneda13@gmail.com>
2022-07-08 13:13:31 -0500

    js09

---
+++ marcoantonio/js/js09/teoria/script_npm.js
@@ -0,0 +1,20 @@
+/**
+ *  Comandos para instalar bootstrap 
+ * 
+ * En consola ******
+ * Entrar a la direccoión de la carpeta donde va estar nuestro trabajo ***
+ * se entra con cd "carepta "
+ * 
+ * luego en consola 
+ * npm init 
+ * next , next , next 
+ * 
+ * En consola colocamos 
+ * 
+ * ntp install bostrap 
+ * 
+ * nos instala la ultima version estable de bootstrap 
+ * 
+ * 
+ * 
+ */
