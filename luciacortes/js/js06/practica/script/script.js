b56921360b672d58de8af6ad9c521999bf178ca4
Lucia Cortes <luciacortesmolina@gmail.com>
2022-07-09 15:13:38 -0500

    add: sesion js06 Javascript HTML DOM

--- luciacortes/js/js06/practica/script/script.js
+++ luciacortes/js/js06/practica/script/script.js
@@ -7,23 +7,35 @@ setInvisible(true);
  */
 function setInvisible(estado){
     const formulario= document.getElementById("formulario");
-    /* let status="visible";
+    /*let status="visible";
     if(estado) status="hidden"
     formulario.elements['limpiar'].style.visibility= status;
-     */
+    formulario.elements['fecha'].style.visibility= status;*/
+    
     
     let status= "inline";
     if(estado) status= "none";
-    formulario.elements['limpiar'].style.display= status
-    formulario.elements['fecha'].style.display= status
+    formulario.elements['limpiar'].style.display= status;
+    formulario.elements['fecha'].style.display= status;
 }
 
-const form= document.getElementById('formulario');
 
+const form= document.getElementById('formulario');
 form.addEventListener('submit', (event) =>{
     event.preventDefault();
     console.log(event);
-    calcularFecha();
+    /**
+     * Implemenar filtro con un 
+     * if (pasaFiltro(){
+     *  calcularFEcja()
+     *  setInvisible(false);
+     * }
+     * else{
+     * //Enviar mensaje a usuario
+     * }
+     * 
+     *      */
+    calcularFecha();    
     setInvisible(false);
 } );
 
@@ -51,11 +63,10 @@ const calcularFecha = () =>{
         formulario.elements['anio'].value,
         formulario.elements['mes'].value-1,
         formulario.elements['dia'].value,
-    )
-
-    formulario.elements['fecha'].value = definirFecha (dia.getDay() );
-}
-
+        )
+        
+        formulario.elements['fecha'].value = definirFecha (dia.getDay() );
+    }
 /**
  //Función para indicar el día y si es laborable
  *  @param {number}  dia 
@@ -88,3 +99,54 @@ function definirFecha(dia){
             return "Día no existente";          
     }
 }
+function definirDias(year,month,day){
+    let anioBis = year % 4;
+    switch (month) {
+      //meses con 31 dias
+      case 0:
+      case 2:
+      case 4:
+      case 6:
+      case 7:
+      case 9:
+      case 11:
+        if (day < 1 || day > 31) {
+          alert("Ingrese un dia dentro del rango 1-31");
+          document.getElementById("dia").value = "";
+          setInvisible(false);
+          return null;
+        }
+        break;
+      case 1:
+        console.log("Dentro del casen deberia dar el año" + year);
+        console.log("año bis" + anioBis);
+        // el año es bisiesto
+        if (anioBis == 0 && (day < 1 || day > 29)) {
+          alert("Ingrese un dia dentro del rango 1-29");
+          document.getElementById("dia").value = "";
+          return null;
+        }
+        // alert("El año no es bisiesto");
+        if (anioBis !== 0 && (day < 1 || day > 28)) {
+          alert("Ingrese un dia dentro del rango 1-28");
+          document.getElementById("dia").value = ""; 
+          return null;
+        }
+        break;
+      // Meses con 30 dias
+      case 3:
+      case 5:
+      case 8:
+      case 10:
+        if (day < 1 || day > 30) {
+          alert("Ingrese un dia dentro del rango 1-31");
+          document.getElementById("dia").value = "";  
+          return null;
+        }
+        break;
+  
+      default:
+        alert("Opcion invalida, verifique que los datos ingresados sean correctos");
+        return null;
+    }
+  }
---
+++ luciacortes/js/js09/teoria/script_npm.js
@@ -0,0 +1,1 @@
+console.log("Sesión tenebrosa de JS09");
