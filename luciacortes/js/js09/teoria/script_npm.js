41272ac7ac66dbef93ccb1aacb8b16621c05f963
Lucia Cortes <luciacortesmolina@gmail.com>
2022-07-09 15:20:41 -0500

    add: sesion JS09 npm

--- luciacortes/js/js09/teoria/script_npm.js
+++ luciacortes/js/js09/teoria/script_npm.js
@@ -1,1 +1,16 @@
 console.log("Sesión tenebrosa de JS09");
+
+// GET request for remote image in node.js
+function adquirirDatos(direccionhttp){
+axios({
+    method: 'get',
+    url: direccionhttp,
+    responseType: 'stream'
+  })
+    .then((response)=> {
+      console.log(response.data.data);
+      //return response.data.data;
+    });
+}
+adquirirDatos('https://reqres.in/api/users?page=2');
+//console.log(await adquirirDatos('https://reqres.in/api/users?page=2'));
