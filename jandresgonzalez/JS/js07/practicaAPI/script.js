61c8b1f252ea0ff6672a4bc15c1e6a03c0ad4ca3
JA Gonzalez <jatgonzalez33@gmail.com>
2022-07-09 15:49:00 -0500

    Add: se harán cambios sobre script de practica

--- jandresgonzalez/JS/js07/practicaAPI/script.js
+++ jandresgonzalez/JS/js07/practicaAPI/script.js
@@ -1,7 +1,4 @@
 console.log("Link");
-const users = [];
-
-
 
 /* for (i = 0; i <7 ; i++) 
 {const node = document.createElement("div")
@@ -18,107 +15,128 @@ document.getElementById("data").appendChild(node);} */
 
 //https://reqres.in/api/users?delay=3
 
-fetch('https://reqres.in/api/users?page=2') 
-    .then(responseJSON => {return responseJSON.json()})
-    .then(usuarios => {
-        
-        //console.log(usuarios.data[0]);
-        for (let i=0; i<usuarios.data.length; i++) {
-            users.push(usuarios.data[i]);
-        }
-        //console.log(objects);
-        /* for (object of objects) {
-            console.log(object);
-        }
- */
-        users.forEach(user => {
-/*             <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text" */
-
-    const col = document.createElement("div");
-    const node = document.createElement("div");
-    const img = document.createElement("img");
-    const card = document.createElement("div");
-    const text = document.createElement("p");
-    text.classList.add("card-text");
-    card.classList.add("card-body");
-    img.setAttribute("src", user.avatar);
-    img.classList.add("card-img-top");
-    node.classList.add("card");
-    col.classList.add("col-sm-4");
-    node.setAttribute("style", "width: 9rem;")
-//    const info = document.createTextNode(`Info: ID --${user.id}-- NAME --${user.first_name} ${user.last_name}-- CONTACT --${user.email}-- ${user.avatar}`);
-    text.innerHTML= `<b>Id#:</b> ${user.id} <br> <b>Name:</b> ${user.first_name} ${user.last_name} <br> <b>Contact:</b> <a href="mailto:${user.email}?Subject=Abue%20hackerman%20page%20inquiry">${user.email}</a>`;
-    card.appendChild(text);
-    //img.appendChild(card);
-    node.appendChild(img);
-    node.appendChild(card);
-    col.appendChild(node);
-
-    document.getElementById("data").appendChild(col);
+function fetchInfo(url) {
+    const users = [];
+    fetch(url) 
+        .then(responseJSON => {return responseJSON.json()})
+        .then(usuarios => {
             
-/*             // Se crea un div
-            const node = document.createElement("div")
-            node.classList.add("row", "col-12", "card");
-            const info = document.createTextNode(`Info: ID --${user.id}-- NAME --${user.first_name} ${user.last_name}-- CONTACT --${user.email}-- ${user.avatar}`)
-            node.appendChild(info);
-            const img = document.createElement("img")
-            img.setAttribute("src", user.avatar)
-            img.classList.add("card-img-top", "w-25")
-            node.appendChild(img);
-            document.getElementById("data").appendChild(node);
-             */
-        });
+            //console.log(usuarios.data[0]);
+            for (let i=0; i<usuarios.data.length; i++) {
+                users.push(usuarios.data[i]);
+            }
+
+            users.forEach(user => {
+
+    
+        const col = document.createElement("div");
+        const node = document.createElement("div");
+        const img = document.createElement("img");
+        const card = document.createElement("div");
+        const text = document.createElement("p");
+        text.classList.add("card-text");
+        card.classList.add("card-body");
+        img.setAttribute("src", user.avatar);
+        img.classList.add("card-img-top");
+        node.classList.add("card", "m-2", "card-hover", ":hover");
+        col.classList.add("col-sm-4", "d-flex", "justify-content-center");
+        node.setAttribute("style", "width: 9rem;")
+    //    const info = document.createTextNode(`Info: ID --${user.id}-- NAME --${user.first_name} ${user.last_name}-- CONTACT --${user.email}-- ${user.avatar}`);
+        text.innerHTML= `<b>Id#:</b> ${user.id} <br> <b>Name:</b> ${user.first_name} ${user.last_name} <br> <b>Contact:</b> <a href="mailto:${user.email}?Subject=Abue%20hackerman%20page%20inquiry">${user.email}</a>`;
+        card.appendChild(text);
+        //img.appendChild(card);
+        node.appendChild(img);
+        node.appendChild(card);
+        col.appendChild(node);
+    
+        document.getElementById("data").appendChild(col);
+                
+    /*             // Se crea un div
+                const node = document.createElement("div")
+                node.classList.add("row", "col-12", "card");
+                const info = document.createTextNode(`Info: ID --${user.id}-- NAME --${user.first_name} ${user.last_name}-- CONTACT --${user.email}-- ${user.avatar}`)
+                node.appendChild(info);
+                const img = document.createElement("img")
+                img.setAttribute("src", user.avatar)
+                img.classList.add("card-img-top", "w-25")
+                node.appendChild(img);
+                document.getElementById("data").appendChild(node);
+                 */
+            });
+    
+    
+    
+    /* 
+            for (object of objects) {
+                console.log(object.id);
+            }
+            for (object of objects) {
+                console.log(object.email);
+            }
+            for (object of objects) {
+                console.log(object.first_name);
+            }
+            for (object of objects) {
+                console.log(object.last_name);
+            }
+            for (object of objects) {
+                console.log(object.avatar);
+            } */        
+    });
+}
 
 
-
-/* 
-        for (object of objects) {
-            console.log(object.id);
-        }
-        for (object of objects) {
-            console.log(object.email);
-        }
-        for (object of objects) {
-            console.log(object.first_name);
-        }
-        for (object of objects) {
-            console.log(object.last_name);
-        }
-        for (object of objects) {
-            console.log(object.avatar);
-        } */        
-});
-
-//callNames();
-function callNames() {
-    let id;
-    let firstName;
-    let lastName;
-    let email;
-    let avatar;
-
-    console.log(users);
-
-    for (person of users) {
-        console.log(person)
-        id = person.id;
-        firstName = person.first_name;
-        lastName = person.last_name;
-        email = person.email;
-        avatar = person.avatar;
-
-        console.log(`Info ${id} ${firstName} ${lastName} ${email} ${avatar}`)
-
-
-        idHTML.innerHTML = id;
-        fNameHTML.innerHTML = firstName;
-        lNameHTML.innerHTML = lastName;
-        emailHTML.innerHTML = email;
-        avatarHTML.innerHTML = `<img src =${avatar}></img>`;
-        
+'https://reqres.in/api/users?page=2'
+function displayInfo() {
+    document.getElementById("buttonStart").style.display = "none";
+    fetchInfo("https://reqres.in/api/users?delay=3");
+    setTimeout(createPagination, 350);
+    }
+    
+    async function createPagination() {
+        document.getElementById("pagination").innerHTML = `               \
+    
+        <div class="container-fluid m-3 d-flex justify-content-center">\
+        <div class="row" class="m-5 p-5 d-flex justify-content-center">\
+          <span class="pagination-strip">\
+            <span>Anterior ---</span>\
+            <!-- Página 1 -->\
+            <span 
+                class="pagination-item"
+                aria-label="Página 1"
+                onclick="paginaUno()">
+                <button class="btn btn-outline-secondary btn-sm"> 1 </button>\
+            </span>\
+            <!-- Página 2 -->\
+            <span
+                class="pagination-item"
+                aria-label="Página 2"
+                onclick="paginaDos()">
+                <button class="btn btn-outline-secondary btn-sm"> 2 </button>\
+            </span>\
+            <!-- Página 3 -->\
+            <span
+                class="pagination-item"
+                aria-label="Página 3"
+                onclick="paginaTres()">
+                <button class="btn btn-outline-secondary btn-sm"> 3 </button>\
+            </span>\
+            <span>--- Siguiente</span>\
+          </span>\
+        </div>\
+      </div>\
+    `;
     }
 
-}
+    function paginaUno() {
+        console.log("clickUno");
+    }
+    function paginaDos() {
+        const last = document.getElementById("data").lastChild;
+        document.getElementById("data").removeChild(last);
+        console.log("clickDos");
+        fetchInfo('https://reqres.in/api/users?page=2');
+    }
+    function paginaTres() {
+        console.log("clickTres");
+    }
