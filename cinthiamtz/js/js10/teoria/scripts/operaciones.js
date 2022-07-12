66dd72f7db47b16f2190e1c27a335af5aa43a08f
cinthia martinez <cinthia.martinez.mtz@outlook.com>
2022-07-12 18:50:38 -0500

    js07

--- cinthiamtz/js/js10/teoria/scripts/operaciones.js
+++ cinthiamtz/js/js10/teoria/scripts/operaciones.js
@@ -6,4 +6,34 @@ function suma (a,b){
 multiplica =(a,b)=> a*b;
 // indica la exportacion 
 
+
+function palabras(palabra){
+
+
+    let arregloUno = ['Math', 'español', 'Programming'];
+//let student1Courses = ['Math', 'English', 'Programming'];
+let arregloDos = ['españoll', 'oro', 'Programming', 'español', 'Math'];
+let max=0
+if( arregloUno.length > arregloDos.length ){
+    max = arregloUno.length;
+}
+else {
+    max = arregloDos.length;
+}
+
+for (let i=0; i<max; i++){
+    for(let j=0; j<max; j++){
+        if(arregloUno [i] == arregloDos [j]){
+        console.log(arregloUno [i]);
+        }        
+    }    
+}
+   
+}
+
+
+
+
 module.exports ={suma, multiplica}
+
+
