38177f872a7f43875b082d119ceff0fb1bd371d9
Lucia Cortes <luciacortesmolina@gmail.com>
2022-07-11 20:40:23 -0500

    clase JS10 pruebas unitarias

---
+++ luciacortes/js/js10/teoria/tests/operacion.palindromo.test.js
@@ -0,0 +1,37 @@
+//Se importan as funciones a probar
+const operacion = require("../scripts/operaciones");
+
+//Este archivo hará las pruebas unitarias para multiplica
+//La funcion test es donde se configura mi prueba unitaria
+//test("Comentario", function callBack);
+
+test ("Debería leer el palíndromo al revés", ()=>{
+    const palindromo = operacion.palindromo("Anita lava la tina");
+    expect(palindromo).toBeTruthy();
+});
+test ("Debería leer el palíndromo al revés", ()=>{
+    const palindromo = operacion.palindromo("Ana");
+    expect(palindromo).toBeTruthy();
+});
+test ("Debería leer el palíndromo al revés", ()=>{
+    const palindromo = operacion.palindromo("Amor a Roma");
+    expect(palindromo).toBeTruthy();
+});
+test ("Debería leer el palíndromo al revés", ()=>{
+    const palindromo = operacion.palindromo("Isaac no ronca asi");
+    expect(palindromo).toBeTruthy();
+});
+test ("Debería leer el palíndromo al revés", ()=>{
+    const palindromo = operacion.palindromo("No es palindromo");
+    expect(palindromo).toBe(false);
+});
+
+test ("Debería leer el palíndromo al revés", ()=>{
+    const palindromo = operacion.palindromo("1 amor, roma 1");
+    expect(palindromo).toBeTruthy();
+});
+
+test ("Debería leer el palíndromo al revés", ()=>{
+    const palindromo = operacion.palindromo("suerte");
+    expect(palindromo).toBe(false);
+});
