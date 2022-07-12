c762ad6ed5d54889a2d3174a3c6c5ac1dbebfc9b
Johana Arredondo <johana.arredondo3402@alumnos.udg.mx>
2022-07-12 09:02:35 -0500

    actualizacion

--- JohanaArredondo/js/js09/teoria/npm.js
+++ JohanaArredondo/js/js09/teoria/npm.js
@@ -1,1 +1,14 @@
 console.log("sesion tenebrosa JS09");
+
+function adquirirDatos(direccionhttp){
+    axios({
+        method: 'get',
+        url: direccionhttp,
+        responseType: 'stream'
+    })
+    .then((response) => {
+        console.log(response.data.data);
+    });
+}
+
+adquirirDatos("https://reqres.in/api/users?page=2");
