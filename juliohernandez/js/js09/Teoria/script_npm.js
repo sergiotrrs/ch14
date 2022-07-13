152b642796067d191975eca5091dd131c0c89f1c
Julio Hernández <julioergocesar@outlook.com>
2022-07-12 19:03:58 -0500

    mod:Funcionalidad lista

--- juliohernandez/js/js09/Teoria/script_npm.js
+++ juliohernandez/js/js09/Teoria/script_npm.js
@@ -1,1 +1,20 @@
-console.log("Sesión tenebrosa JS09, muajajajaja")
+console.log("Sesión tenebrosa JS09, muajajajaja");
+
+// GET request for remote image in node.js
+function adquirirDatos(direccionHTTP){
+
+    return axios({
+        method: 'get',
+        url: direccionHTTP,
+        responseType: 'stream'
+      })
+        .then((response)=> {
+          //console.log(response.data.data)
+          
+        });
+
+    
+        //No es necesario parse-arlo a json
+}
+
+console.log(adquirirDatos("https://reqres.in/api/users?page=2"));
