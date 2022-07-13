384ee4e8c022941457b098908d94c3ff7359e3cc
Azucena Ceniceros <azucg99@gmail.com>
2022-07-12 20:35:19 -0500

    Añadí el archivo de java

---
+++ azucenacg/java/java02/src/org/generation/HolaMundo.java
@@ -0,0 +1,12 @@
+package org.generation;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("¡Hola Mundo 2!");
+		System.out.println("¡Hola Mundo 3!");
+		System.out.println("¡Hola Mundo 4!");
+	}
+
+}
