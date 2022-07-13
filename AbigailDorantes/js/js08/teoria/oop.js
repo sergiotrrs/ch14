3360a7aeac2091e75819eac6db13f5327900f0be
Abigail Dorantes <abydorantes@hotmail.com>
2022-07-12 21:33:11 -0500

    Agrego tarea ApiFetch js07

---
+++ AbigailDorantes/js/js08/teoria/oop.js
@@ -0,0 +1,13 @@
+console.log("Sesion JS08 OOP");
+
+const elotes = {
+    type: "vaso",
+    color: "amarillo", 
+    size: "chico", 
+    color: 25, 
+    preparado:true
+};
+
+console.log("Venga por su elote")
+console.log("Tenemos elote tipo:" + elotes.type)
+
