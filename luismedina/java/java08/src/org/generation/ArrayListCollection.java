9cccf0aef73e062fd7bfc19798bfd798d05515a6
Luis Medina <luisdavid220800@gmail.com>
2022-07-21 17:35:32 -0500

    Add: Ejercicios clase Java

--- luismedina/java/java08/src/org/generation/ArrayListCollection.java
+++ luismedina/java/java08/src/org/generation/ArrayListCollection.java
@@ -11,19 +11,55 @@ public class ArrayListCollection {
 		 * coleccion de elementos. De las colecciones es la que mejor rendimiento tiene.
 		 */
 //		Las coleeciones no usan valores primitivos, usan los tipos envolventes
-		//ejemplo int->Integer
-		
-		//Definimos un arrayList de tipo String
+		// ejemplo int->Integer
+
+		// Definimos un arrayList de tipo String
 		ArrayList<String> cars = new ArrayList<String>();
-		
-		//Agregamos elementos al arreglo
+		ArrayList<Integer> edades = new ArrayList<Integer>();
+		// Agregamos elementos al arreglo
 		cars.add("Volvo");
 		cars.add("BMW");
 		cars.add("Vochito");
 		cars.add("Volvo");
 		cars.add("Mazda");
-		
 		System.out.println(cars);
+		// Agregar un elemento en una posicion indicada
+		cars.add(1, "Mercedez Benz");
+		System.out.println(cars);
+		System.out.println(cars.size());
+
+		// Desplegar una posicion indicada
+		String autoLucia = cars.get(1);
+		System.out.println("El auto de Lucia es: " + autoLucia);
+
+		// Eliminar una posicion de la coleccion, utilizamos remove
+		String elektra = cars.remove(2);
+		System.out.println(cars);
+		System.out.println("Elektra se lleva el: " + elektra);
+
+		ArrayList<String> autos = new ArrayList<String>();
+		autos = (ArrayList<String>) cars.clone();
+		System.out.println(autos);
+		System.out.println(cars);
+		System.out.println(autos.hashCode());
+		System.out.println(cars.hashCode());
+		autos.clear();
+		System.out.println("Cars"+cars);
+		System.out.println("Autos"+autos);
+		
+		for (int i = 0; i < cars.size(); i++) {
+			System.out.println("Te regalo el auto: "+cars.get(i));
+		}
+		
+		for (String carros : cars) {
+			System.out.println("Te regalo el auto: "+carros);
+		}
+
+		cars.forEach(elemento->{
+			System.out.println("Auto: "+ elemento);
+		});
+		
+		
 	}
 
 }
