d2ad75af04cb78436317d73832e8739d00150d9b
Margarita Flores <mfloreszatarain95@gmail.com>
2022-07-25 16:40:02 -0600

    share: codigos

--- margaritaflores/java/java10/src/org/generation/mascota/Michi.java
+++ margaritaflores/java/java10/src/org/generation/mascota/Michi.java
@@ -1,18 +1,31 @@
 package org.generation.mascota;
 
-public class Perrito {
+public class Michi {
 	private byte edad;
 	private String nombre;
 	private String raza;
 	private short size;
+	private int vidas;
 	
-	public Perrito (byte edad, String nombre, String raza, short size){
+	public Michi (byte edad, String nombre, String raza, short size, int vidas){
 		this.edad = edad;
 		this.nombre= nombre;
 		this.raza = raza;
 		this.size = size;
+		this.vidas = vidas;
 	}
 	
+	
+	public int getVidas() {
+		return vidas;
+	}
+
+
+	public void setVidas(int vidas) {
+		this.vidas = vidas;
+	}
+
+
 	public void setEdad(byte edad) {
 		this.edad = edad;
 	}
