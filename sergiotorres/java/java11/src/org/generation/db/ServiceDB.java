ec3f6dc4e72b1c7ab619c0f2dc8f8aed8b770fd0
Sergio Torres <sergio.torres@idr.edu.mx>
2022-07-28 16:42:07 -0500

    chore: sesión de pruebas unitarias

--- sergiotorres/java/java11/src/org/generation/db/ServiceDB.java
+++ sergiotorres/java/java11/src/org/generation/db/ServiceDB.java
@@ -3,14 +3,29 @@ package org.generation.db;
 public class ServiceDB {
 
 	public static void main(String[] args) {
-		GenericDb conexion = new ConexionMySQL("localhost", 3039);
+		boolean developing = false;
+		GenericDb conexion;
 		
+		if (developing) {
+			conexion = new ConexionMySQL("localhost", 3039); 
+		}
+		else {
+			conexion = new ConexionOracle("10.0.1.50", 5090, "Permitido");
+		}
+							
 		System.out.println(conexion.insertar(528));
 		System.out.println(conexion.insertar(10));
 		System.out.println(conexion.insertar(100));
 		System.out.println(conexion.actualizar());
-		System.out.println(conexion.eliminar());
+		System.out.println(conexion.eliminar());		
 		
+		/**
+		 * Actividad:
+		 * 
+		 * Del ejercio anterior de clases, implementar
+		 * una interfaz y una clase abastracta, implementar y heredarla.
+		 * 
+		 */
 		
 
 	}
---
+++ sergiotorres/java/java12/src/org/generation/test/TestSuma.java
@@ -0,0 +1,55 @@
+package org.generation.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.generation.aritmetica.Aritmetica;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+/**
+ * Las pruebas unitarias son pruebas automatizadas
+ * escritas y ejecutas para garantizar que una 
+ * sección de nuestra aplicación(unidad) cumpla
+ * con su diseño y se comporte según lo previsto.
+ * 
+ * En el desarrollo basado en pruebas (TDD Test-Driven Development)
+ * las prueba sunitarias se crean antes de 
+ * que se escriba el código. Cuando pasan las
+ * pruebas, ese código se considera completo.
+ * 
+ * Las pruebas unitarias permiten al desarrollador
+ * refactorizar el código o actualizar bibliotecas
+ * y asegurarse que el módulo siga funcionando.
+ * 
+ * @author TuX3
+ *
+ */
+class TestSuma {
+	Aritmetica aritmetica;
+	
+	@BeforeEach
+	void setUp() {
+		aritmetica = new Aritmetica();
+	}
+
+	@Test
+	@DisplayName("Sumatoria de números enteros")
+	void testSuma() {
+		assertEquals(10, aritmetica.suma(6, 4), "error en sumatoria de int positivo" );
+	}
+
+	@Test
+	@DisplayName("Sumatoria de números de punto flotante")
+	void testSumaDouble() {
+		assertEquals(11.5, aritmetica.suma(6, 5.5), "error en sumatoria double positivo" );
+	}
+	
+	@Test
+	@DisplayName("Verifica si la sumatoria es positiva")
+	void testIsPositive() {
+		assertTrue(aritmetica.suma(16, 5)>=0, "error en sumatoria positiva" );
+	}
+	
+	
+
+}
