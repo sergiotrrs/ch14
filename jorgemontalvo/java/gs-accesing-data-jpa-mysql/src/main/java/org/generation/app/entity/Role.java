f1f8be2f754edb61c28a713d1ffbc2566ddb2e8d
Jorge Montalvo <jorgemontalvo96@gmail.com>
2022-08-14 20:06:13 -0500

    add: archivos java web

---
+++ jorgemontalvo/java/gs-accesing-data-jpa-mysql/src/main/java/org/generation/app/entity/Role.java
@@ -0,0 +1,23 @@
+package org.generation.app.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Role implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="role_id")
+	private Long roleId;
+	@Column(name="role_type")
+	private String roleType;
+	@Column(name="description")
+	private String description;
+
+
+}
